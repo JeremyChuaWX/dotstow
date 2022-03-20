71cd4dc070117ab667157028056493a4798351ad
JeremyChuaWX <chua.jeremy.wx@gmail.com>
2022-03-20 10:59:54 +0800

    file and buffer exclusions for indent-blankline

--- nvim/.config/nvim/lua/plugin-configs/blankline.lua
+++ nvim/.config/nvim/lua/plugin-configs/blankline.lua
@@ -6,4 +6,16 @@ end
 blankline.setup {
     show_current_context = true,
     show_current_context_start = true,
+    show_first_indent_level = false,
+    filetype_exclude = {
+        "help",
+        "neogit",
+        "terminal",
+        "lspinfo",
+        "packer",
+    },
+    buftype_exclude = {
+        "terminal",
+        "nofile",
+    },
 }
