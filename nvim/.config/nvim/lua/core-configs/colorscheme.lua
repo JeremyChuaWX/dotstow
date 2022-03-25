294c2c1e208f199d30c481e2f9664dea4fcc59f6
JeremyChuaWX <chua.jeremy.wx@gmail.com>
2022-03-26 00:03:57 +0800

    new way to set colorscheme

--- nvim/.config/nvim/lua/core-configs/colorscheme.lua
+++ nvim/.config/nvim/lua/core-configs/colorscheme.lua
@@ -5,10 +5,12 @@ let g:everforest_better_performance = 1
 
 " tokyo night
 let g:tokyonight_style = "night"
-
-try
-    colorscheme kanagawa
-catch /^Vim\%((\a\+)\)\=:E185/
-    colorscheme default
-endtry
 ]]
+
+local colorscheme = "kanagawa"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+    vim.notify("colorscheme " .. colorscheme .. " not found!")
+    return
+end
