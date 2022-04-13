7328d275c0e0b93b3bcc43404becbcb7b927f7bb
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-04-13 09:49:12 +0800

    add auto install lsp servers for nvimlspinstaller

--- nvim/.config/nvim/lua/plugin-configs/lsp/lsp-installer.lua
+++ nvim/.config/nvim/lua/plugin-configs/lsp/lsp-installer.lua
@@ -3,6 +3,22 @@ if not status_ok then
     return
 end
 
+local servers = {
+    "sumneko_lua",
+    "pyright",
+    "tsserver",
+    "emmet_ls",
+    "jsonls",
+}
+
+for _, name in pairs(servers) do
+    local server_is_found, server = lsp_installer.get_server(name)
+    if server_is_found and not server:is_installed() then
+        print("Installing " .. name)
+        server:install()
+    end
+end
+
 -- register a handler that will be called for all installed servers
 -- you may also register handlers on specific server instances instead
 lsp_installer.on_server_ready(function(server)
