2a94feb91ce3dc264b855caa3597ceed373cd7ba
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-04-14 09:13:22 +0800

    fix nvim cmp

--- nvim/.config/nvim/lua/plugin-configs/nvimcmp.lua
+++ nvim/.config/nvim/lua/plugin-configs/nvimcmp.lua
@@ -21,7 +21,7 @@ cmp.setup({
             luasnip.lsp_expand(args.body)
         end,
     },
-    mapping = {
+    mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
         ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
         ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
@@ -57,7 +57,7 @@ cmp.setup({
             end
         end,
         { "i", "s" }),
-    },
+    }),
     sources = {
         { name = 'nvim_lsp_signature_help' },
         { name = 'luasnip' },
@@ -70,6 +70,7 @@ cmp.setup({
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
     sources = {
         { name = 'buffer' }
     }
@@ -77,6 +78,7 @@ cmp.setup.cmdline('/', {
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
     sources = {
         { name = 'path' },
         { name = 'cmdline' },
