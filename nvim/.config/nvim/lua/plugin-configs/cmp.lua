f45e347e0fa08faf77d619d1c53984b5e7c65f12
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-04-16 12:24:06 +0800

    cmp config

--- nvim/.config/nvim/lua/plugin-configs/cmp.lua
+++ nvim/.config/nvim/lua/plugin-configs/cmp.lua
@@ -8,11 +8,11 @@ if not snip_status_ok then
     return
 end
 
-require("luasnip/loaders/from_vscode").lazy_load()
+require('luasnip/loaders/from_vscode').lazy_load()
 
 local check_backspace = function()
-    local col = vim.fn.col "." - 1
-    return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+    local col = vim.fn.col '.' - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match '%s'
 end
 
 cmp.setup({
@@ -22,9 +22,6 @@ cmp.setup({
         end,
     },
     mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
         ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
         ['<C-e>'] = cmp.mapping({
             i = cmp.mapping.abort(),
@@ -33,7 +30,7 @@ cmp.setup({
         -- Accept currently selected item.
         -- Set `select` to `false` to only confirm explicitly selected items.
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
+        ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
             elseif luasnip.expandable() then
@@ -46,8 +43,8 @@ cmp.setup({
                 fallback()
             end
         end,
-        { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
+        { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
             elseif luasnip.jumpable(-1) then
@@ -56,7 +53,7 @@ cmp.setup({
                 fallback()
             end
         end,
-        { "i", "s" }),
+        { 'i', 's' }),
     }),
     sources = {
         { name = 'nvim_lsp_signature_help' },
