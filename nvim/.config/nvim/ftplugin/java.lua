1c0d39b5b5482984ea19317e3f59d877216b7360
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-04-17 20:26:55 +0800

    enable jdtls formatting

--- nvim/.config/nvim/ftplugin/java.lua
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -110,7 +110,7 @@ local config = {
         includeDecompiledSources = true,
       },
       format = {
-        enabled = false,
+        enabled = true,
         -- settings = {
         --   profile = "asdf"
         -- }
