e99065bdf7f3f720709df87dd31d1a90f5c8cfed
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-04-21 17:54:39 +0800

    add gitsigns to null-ls for hunk code actions

--- nvim/.config/nvim/lua/plugin-configs/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/plugin-configs/lsp/null-ls.lua
@@ -5,11 +5,13 @@ end
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
+local actions = null_ls.builtins.code_actions
 
 null_ls.setup {
     debug = false,
     sources = {
         formatting.black.with({ extra_args = { "--fast" } }),
         formatting.prettier.with({ extra_args = { "--no-semi" } }),
+        actions.gitsigns,
     },
 }
