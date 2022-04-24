71f8397bbf0c23566d3320f1f72a2c873008d065
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-04-24 08:46:50 +0800

    lsp settings

--- nvim/.config/nvim/lua/plugin-configs/lsp/settings/sumneko_lua.lua
+++ nvim/.config/nvim/lua/plugin-configs/lsp/settings/sumneko_lua.lua
@@ -1,19 +1,14 @@
-local runtime_path = vim.split(package.path, ";")
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
 return {
     settings = {
         Lua = {
-            runtime = {
-                version = "LuaJIT",
-                path = runtime_path,
-            },
             diagnostics = {
                 globals = { "vim" },
             },
             workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
+                library = {
+                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                    [vim.fn.expand("config") .. "/lua"] = true,
+                },
             },
             telemetry = {
                 enable = false,
