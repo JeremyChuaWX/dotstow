5e6a1d224a591cb1e58683e4d150a5d30a7d11bd
JeremyChuaWX <chua.jeremy.wx@gmail.com>
2022-04-30 23:05:04 +0800

    disable nvim gps and edit lualine filename path

--- nvim/.config/nvim/lua/plugin-configs/lualine.lua
+++ nvim/.config/nvim/lua/plugin-configs/lualine.lua
@@ -1,52 +1,60 @@
 local lualine_ok, lualine = pcall(require, "lualine")
 if not lualine_ok then
-    return
+	return
 end
 
-local gps_ok, gps = pcall(require, "nvim-gps")
-if not gps_ok then
-    return
-end
+-- local gps_ok, gps = pcall(require, "nvim-gps")
+-- if not gps_ok then
+-- 	return
+-- end
 
-lualine.setup {
-    options = {
-        globalstatus = true,
-        icons_enabled = true,
-        theme = "auto",
-        -- component_separators = { left = "", right = ""},
-        component_separators = "",
-        -- section_separators = { left = "", right = ""},
-        section_separators = "",
-        disabled_filetypes = {},
-        always_divide_middle = true,
-    },
+lualine.setup({
+	options = {
+		globalstatus = true,
+		icons_enabled = true,
+		theme = "auto",
+		-- component_separators = { left = "", right = ""},
+		component_separators = "",
+		-- section_separators = { left = "", right = ""},
+		section_separators = "",
+		disabled_filetypes = {},
+		always_divide_middle = true,
+	},
 
-    -- +-------------------------------------------------+
-    -- | A | B | C                             X | Y | Z |
-    -- +-------------------------------------------------+
+	-- +-------------------------------------------------+
+	-- | A | B | C                             X | Y | Z |
+	-- +-------------------------------------------------+
 
-    sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "diagnostics" },
-        lualine_c = {
-            "%1n:", "filename",
-            { gps.get_location, cond = gps.is_available },
-        },
-        lualine_x = { "filetype" },
-        lualine_y = { "branch", "diff" },
-        lualine_z = { "%l/%L" }
-    },
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "diagnostics" },
+		lualine_c = {
+			"%1n:",
+			{
+				"filename",
+				path = 2,
+				shorting_target = 999, -- Shortens path to leave 40 spaces in the window
+			},
+			-- {
+			-- 	gps.get_location,
+			-- 	cond = gps.is_available,
+			-- },
+		},
+		lualine_x = { "filetype" },
+		lualine_y = { "branch", "diff" },
+		lualine_z = { "%l/%L" },
+	},
 
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {}
-    },
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = {},
+	},
 
-    tabline = {},
+	tabline = {},
 
-    extensions = {},
-}
+	extensions = {},
+})
