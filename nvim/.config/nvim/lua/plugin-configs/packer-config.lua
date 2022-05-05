1c5ac3f6af782f687ad9de7603c6550c3a2af172
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-05-05 17:24:23 +0800

    incsearch in cmp fixed

--- nvim/.config/nvim/lua/plugin-configs/packer-config.lua
+++ nvim/.config/nvim/lua/plugin-configs/packer-config.lua
@@ -98,10 +98,7 @@ return packer.startup(function(use)
     use "rafamadriz/friendly-snippets"
 
     -- cmp
-    use {
-        "hrsh7th/nvim-cmp",
-        commit = "bba6fb67fdafc0af7c5454058dfbabc2182741f4",
-    }
+    use "hrsh7th/nvim-cmp"
 
     -- cmp sources
     use "hrsh7th/cmp-buffer"
