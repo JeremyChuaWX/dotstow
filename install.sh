cdc7fc3eda1476b47a0edfd808951b4960e9222b
JeremyChuaWX <chua.jeremy.wx@gmail.com>
2022-05-05 12:00:45 +0800

    google java format install script

--- install.sh
+++ install.sh
@@ -85,11 +85,11 @@ if [[ $OSTYPE = "darwin"* ]] then
     echo "===== Done =====\n"
 
     echo "\n===== Installing utilities ====="
-    sudo -u $SUDO_USER brew install coreutils fzf git ripgrep fd tree tmux stow
+    sudo -u $SUDO_USER brew install coreutils fzf git ripgrep fd tree tmux stow google-java-format
     echo "===== Done =====\n"
 
     echo "\n===== Installing languages ====="
-    sudo -u $SUDO_USER brew install node python3 go
+    sudo -u $SUDO_USER brew install node python3 go openjdk
     echo "===== Done =====\n"
 
 fi
