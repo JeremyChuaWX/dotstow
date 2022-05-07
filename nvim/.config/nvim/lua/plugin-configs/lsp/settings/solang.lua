921a17014c3bf54cfd617ea928b826f7aca9587a
JeremyChuaWX <chua.jeremy.wx@gmail.com>
2022-05-07 11:25:24 +0800

    rename to lsp-setup

--- nvim/.config/nvim/lua/plugin-configs/lsp/settings/solang.lua
+++ nvim/.config/nvim/lua/plugin-configs/lsp/settings/solang.lua
@@ -1,4 +1,5 @@
 -- targets can be one of ewasm, generic, sabre, solana, substrate
 return {
 	cmd = { "solang", "--language-server", "--target", "ewasm", "--importpath", "node_modules/" },
+    root_dir = require("lspconfig").util.get_json_ancestor,
 }
