4b45f4a6b79e834b08bc9a470ed858f831b667a7
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-05-08 09:41:02 +0800

    edit nvimtree config

--- nvim/.config/nvim/lua/plugin-configs/nvimtree.lua
+++ nvim/.config/nvim/lua/plugin-configs/nvimtree.lua
@@ -1,6 +1,31 @@
+vim.g.nvim_tree_add_trailing = 1
+vim.g.nvim_tree_icons = {
+	default = "",
+	symlink = "",
+	git = {
+		unstaged = "[]",
+		staged = "[ﰶ]",
+		unmerged = "[]",
+		renamed = "[➜]",
+		untracked = "[]",
+		deleted = "[﯀]",
+		ignored = "[]",
+	},
+	folder = {
+		arrow_open = "",
+		arrow_closed = "",
+		default = "",
+		open = "",
+		empty = "",
+		empty_open = "",
+		symlink = "",
+		symlink_open = "",
+	},
+}
+
 require("nvim-tree").setup({
 	auto_reload_on_write = true,
-	disable_netrw = false,
+	disable_netrw = true,
 	hijack_cursor = false,
 	hijack_netrw = true,
 	hijack_unnamed_buffer_when_opening = false,
@@ -64,12 +89,12 @@ require("nvim-tree").setup({
 	},
 	filters = {
 		dotfiles = false,
-		custom = {},
+		custom = { "^\\.git$", "^node_modules$" },
 		exclude = {},
 	},
 	git = {
 		enable = true,
-		ignore = true,
+		ignore = false,
 		timeout = 400,
 	},
 	actions = {
@@ -115,3 +140,46 @@ local set = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 set("", "<leader>e", ":NvimTreeToggle<CR>", opts)
+
+-- mappings when in NvimTree buffer
+-- local list = {
+-- 	{ key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
+-- 	{ key = "<C-e>", action = "edit_in_place" },
+-- 	{ key = { "O" }, action = "edit_no_picker" },
+-- 	{ key = { "<2-RightMouse>", "<C-]>" }, action = "cd" },
+-- 	{ key = "<C-v>", action = "vsplit" },
+-- 	{ key = "<C-x>", action = "split" },
+-- 	{ key = "<C-t>", action = "tabnew" },
+-- 	{ key = "<", action = "prev_sibling" },
+-- 	{ key = ">", action = "next_sibling" },
+-- 	{ key = "P", action = "parent_node" },
+-- 	{ key = "<BS>", action = "close_node" },
+-- 	{ key = "<Tab>", action = "preview" },
+-- 	{ key = "K", action = "first_sibling" },
+-- 	{ key = "J", action = "last_sibling" },
+-- 	{ key = "I", action = "toggle_git_ignored" },
+-- 	{ key = "H", action = "toggle_dotfiles" },
+-- 	{ key = "R", action = "refresh" },
+-- 	{ key = "a", action = "create" },
+-- 	{ key = "d", action = "remove" },
+-- 	{ key = "D", action = "trash" },
+-- 	{ key = "r", action = "rename" },
+-- 	{ key = "<C-r>", action = "full_rename" },
+-- 	{ key = "x", action = "cut" },
+-- 	{ key = "c", action = "copy" },
+-- 	{ key = "p", action = "paste" },
+-- 	{ key = "y", action = "copy_name" },
+-- 	{ key = "Y", action = "copy_path" },
+-- 	{ key = "gy", action = "copy_absolute_path" },
+-- 	{ key = "[c", action = "prev_git_item" },
+-- 	{ key = "]c", action = "next_git_item" },
+-- 	{ key = "-", action = "dir_up" },
+-- 	{ key = "s", action = "system_open" },
+-- 	{ key = "q", action = "close" },
+-- 	{ key = "g?", action = "toggle_help" },
+-- 	{ key = "W", action = "collapse_all" },
+-- 	{ key = "S", action = "search_node" },
+-- 	{ key = ".", action = "run_file_command" },
+-- 	{ key = "<C-k>", action = "toggle_file_info" },
+-- 	{ key = "U", action = "toggle_custom" },
+-- }
