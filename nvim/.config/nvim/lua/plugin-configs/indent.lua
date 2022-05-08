7e47ab5e209f8d609fcc74c4db633669db6bf7f2
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-05-08 09:40:22 +0800

    indent blankline config

--- nvim/.config/nvim/lua/plugin-configs/indent.lua
+++ nvim/.config/nvim/lua/plugin-configs/indent.lua
@@ -7,16 +7,24 @@ blankline.setup {
     show_current_context = true,
     show_current_context_start = true,
     show_first_indent_level = false,
+    show_trailing_blackline_indent = false,
     filetype_exclude = {
+        "NvimTree",
         "help",
         "neogit",
         "terminal",
         "lspinfo",
         "packer",
+        "markdown",
+        "txt",
+        "git",
+        "gitcommit",
+        "TelescopePrompt",
+        "packer",
+        "", -- for buffers without a file type
     },
     buftype_exclude = {
         "terminal",
         "nofile",
-        "guihua",
     },
 }
