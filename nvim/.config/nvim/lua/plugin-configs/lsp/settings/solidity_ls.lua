4ae9145e17f9397f435d8471f3aa71f2af4058a4
JeremyChuaWX <chua.jeremy.wx@gmail.com>
2022-05-12 13:54:26 +0800

    solidity ls root detection

--- nvim/.config/nvim/lua/plugin-configs/lsp/settings/solidity_ls.lua
+++ nvim/.config/nvim/lua/plugin-configs/lsp/settings/solidity_ls.lua
@@ -1,4 +1,14 @@
 return {
+    root_dir = function (fname)
+        local git_root = require("lspconfig").util.find_git_ancestor(fname)
+        local pkg_root = require("lspconfig").util.find_package_json_ancestor(fname)
+
+        if git_root then
+            return git_root
+        elseif pkg_root then
+            return pkg_root
+        end
+    end,
 	settings = {
 		solidity = {
 			packageDefaultDependenciesContractDirectory = "",
