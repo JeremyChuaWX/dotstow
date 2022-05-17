36b85a4c2139f1457a7488ab8487f32bc6e4866a
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-05-17 10:09:34 +0800

    null ls config

--- nvim/.config/nvim/lua/plugin-configs/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/plugin-configs/lsp/null-ls.lua
@@ -1,42 +1,46 @@
 local status_ok, null_ls = pcall(require, "null-ls")
 if not status_ok then
-    return
+	return
 end
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local actions = null_ls.builtins.code_actions
 
-null_ls.setup {
-    debug = false,
-    diagnostics_format = "#{m} (#{s})",
-    update_in_insert = true,
-    on_attach = require("plugin-configs.lsp.handlers").on_attach,
+null_ls.setup({
+	debug = false,
+	diagnostics_format = "[#{c}] #{m} (#{s})",
+	update_in_insert = true,
+	on_attach = require("plugin-configs.lsp.handlers").on_attach,
 
-    sources = {
-        -- general
-        actions.gitsigns,
+	sources = {
+		-- general
+		actions.gitsigns,
 
-        -- zsh
-        diagnostics.zsh,
+		-- zsh
+		diagnostics.zsh,
 
-        -- lua
-        formatting.stylua,
+		-- lua
+		formatting.stylua,
 
-        --python
-        formatting.black.with {
-            extra_args = { "--fast" },
-        },
-        diagnostics.flake8,
+		--python
+		formatting.black.with({
+			extra_args = { "--fast" },
+		}),
+		diagnostics.flake8,
 
-        -- java
-        formatting.google_java_format,
+		-- java
+		formatting.google_java_format,
 
-        -- js family
-        formatting.prettier.with {
-            extra_args = { "--no-semi" },
-        },
-        diagnostics.eslint,
-        actions.eslint,
-    },
-}
+		-- js family
+		formatting.prettier.with({
+			extra_args = { "--no-semi" },
+		}),
+		diagnostics.eslint.with({
+			only_local = true,
+		}),
+		actions.eslint.with({
+			only_local = true,
+		}),
+	},
+})
