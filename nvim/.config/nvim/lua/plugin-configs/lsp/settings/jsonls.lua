25df61cb730bce79a572e169f073eefed173de64
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-05-18 14:54:44 +0800

    update settings for jsonls to recommended ones

--- nvim/.config/nvim/lua/plugin-configs/lsp/settings/jsonls.lua
+++ nvim/.config/nvim/lua/plugin-configs/lsp/settings/jsonls.lua
@@ -1,17 +1,13 @@
 local status_ok, schemastore = pcall(require, "schemastore")
 if not status_ok then
-    return
+	return
 end
 
 return {
-    settings = {
-        json = {
-            schemas = schemastore.json.schemas {
-                -- select = {
-                --     "package.json",
-                --     "tsconfig.json",
-                -- },
-            },
-        },
-    },
+	settings = {
+		json = {
+			schemas = schemastore.json.schemas(),
+			validate = { enable = true },
+		},
+	},
 }
