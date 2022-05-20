40cdc5afbb2a3ad6637cccc18fa9ce683b656174
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-05-20 10:57:15 +0800

    solc include path for lsp not working yet

--- nvim/.config/nvim/lua/plugin-configs/lsp/settings/solc.lua
+++ nvim/.config/nvim/lua/plugin-configs/lsp/settings/solc.lua
@@ -1,6 +1,6 @@
 -- targets can be one of ewasm, generic, sabre, solana, substrate
 return {
-	cmd = { "solc", "--base-path", ".", "--include-path", "node_modules/", "--lsp" },
+	-- cmd = { "solc", "--base-path", ".", "--include-path", "node_modules/", "--lsp" }, [not supported by solc yet]
 	root_dir = function(fname)
 		local git_root = require("lspconfig").util.find_git_ancestor(fname)
 		local pkg_root = require("lspconfig").util.find_package_json_ancestor(fname)
