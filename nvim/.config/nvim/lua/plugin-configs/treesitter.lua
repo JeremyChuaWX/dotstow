3c6b1faa626c3004ee7d90498e9393ab9a3c5ef0
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-05-26 10:03:31 +0800

    remove yati

--- nvim/.config/nvim/lua/plugin-configs/treesitter.lua
+++ nvim/.config/nvim/lua/plugin-configs/treesitter.lua
@@ -1,31 +1,28 @@
 local status_ok, ts = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-    return
+	return
 end
 
-ts.setup {
-    ensure_installed = "all",
-    ignore_install = { "phpdoc" },
-    highlight = {
-        enable = true,
-        -- additional_vim_highlighting = false,
-    },
-    indent = {
-        enable = true,
-        disable = {
-            "yaml",
-        },
-    },
-    autopairs = {
-        enable = true,
-    },
-    autotag = {
-        enable = true,
-    },
-    yati = {
-        enable = false,
-    }
-}
+ts.setup({
+	ensure_installed = "all",
+	ignore_install = { "phpdoc" },
+	highlight = {
+		enable = true,
+		-- additional_vim_highlighting = false,
+	},
+	indent = {
+		enable = true,
+		disable = {
+			"yaml",
+		},
+	},
+	autopairs = {
+		enable = true,
+	},
+	autotag = {
+		enable = true,
+	},
+})
 
 -- folding
 vim.opt.foldmethod = "expr"
