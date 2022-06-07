145d2491a31a97edb1df3562285d21b8fc06858d
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-06-07 09:50:22 +0800

    select choice node mapping in luasnip

--- nvim/.config/nvim/lua/plugin-configs/luasnip.lua
+++ nvim/.config/nvim/lua/plugin-configs/luasnip.lua
@@ -7,3 +7,6 @@ require("luasnip.loaders.from_vscode").lazy_load()
 
 luasnip.filetype_extend("typescriptreact", { "html" })
 luasnip.filetype_extend("javascriptreact", { "html" })
+
+local set = vim.keymap.set
+set("i", "<C-u>", require("luasnip.extras.select_choice"))
