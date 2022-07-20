165774265f1c9e6f756ed161bb206f3b86673a23
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-07-21 02:04:20 +0800

    ignore null ls fidget

--- nvim/.config/nvim/lua/user/lsp/fidget.lua
+++ nvim/.config/nvim/lua/user/lsp/fidget.lua
@@ -42,6 +42,11 @@ fidget.setup({
 			)
 		end,
 	},
+	sources = {
+		["null-ls"] = {
+			ignore = true,
+		},
+	},
 	debug = {
 		logging = false, -- whether to enable logging, for debugging
 		strict = false, -- whether to interpret LSP strictly
