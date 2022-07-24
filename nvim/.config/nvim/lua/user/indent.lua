0c44ddcd99dfc7d7fd11b9841f3e20d80056fdbd
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-07-24 09:31:44 +0800

    indent highlighting

--- nvim/.config/nvim/lua/user/indent.lua
+++ nvim/.config/nvim/lua/user/indent.lua
@@ -4,9 +4,10 @@ if not status_ok then
 end
 
 blankline.setup({
+    char = "┃",
     show_current_context = true,
-    show_current_context_start = true,
-    show_first_indent_level = false,
+    -- show_current_context_start = true,
+    show_first_indent_level = true,
     show_trailing_blackline_indent = false,
     filetype_exclude = {
         "NvimTree",
@@ -28,3 +29,7 @@ blankline.setup({
         "nofile",
     },
 })
+
+vim.cmd("highlight IndentBlanklineChar guifg=#3f3e3a gui=nocombine")
+vim.cmd("highlight IndentBlanklineContextChar guifg=#ffbf00 gui=nocombine")
+-- vim.cmd("highlight IndentBlanklineContextStart guisp=#ffbf00 gui=nocombine,underline cterm=underline")
